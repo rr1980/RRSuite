2552d1c46f2c5f85e614b9d9fb9e66df73c842ce
rr1980 <rener1980@googlemail.com>
2016-09-19 20:27:40 +0200

    a

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,33 @@
-﻿
-window.Services = {};
+﻿window.Services = {};
 var socket = window.io();
-socket.on("eval", function (msg) {          //socket.emit('rdy');
+socket.on("eval", function(msg) { //socket.emit('rdy');
     eval(msg.data);
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-    $("#blub").click(function () {
+    $(function() {
+        $(".rr-nav-content").on('mousewheel', function(event, delta) {
+            this.scrollLeft -= (delta * 60);
+            event.preventDefault();
+        });
+    });
+
+
+    $("#blub").click(function() {
         $.ajax({
             url: "/getFollows",
             type: "GET",
             cahce: false
-        }).done(function (data) {
+        }).done(function(data) {
             console.debug(data);
-            $(".rr-content").html("");
+            $(".rr-nav-content").html("");
             for (var f in data) {
-                var str = "<img src='" + data[f].preview.medium + "' />";
-                $(".rr-content").append(str);
+                var str = "<img class='rr-nav-content-img' src='" + data[f].preview.medium + "' />";
+                //var img = $("<img class='rr-nav-content-img' />").attr('src', data[f].preview.medium);
+                $(".rr-nav-content").append(str);
             }
+            $(".rr-nav").toggleClass('rr-nav-open');
         });
     });
 
