2009a630db5ca9e64e6af89b386ce8ae7892b568
rr1980 <rener1980@googlemail.com>
2016-09-20 17:40:10 +0200

    a

--- public/javascripts/viewmodels/headerViewmodel.js
+++ public/javascripts/viewmodels/headerViewmodel.js
@@ -1,41 +1,11 @@
 var headerViewmodel = function(data) {
     var self = this;
-
+    self.items = ko.observableArray();
     ko.mapping.fromJS(data, {}, self);
 
-    // $(function() {
-    //     $(".rr-nav-content").on('mousewheel', function(event, delta) {
-    //         this.scrollLeft -= (delta * 60);
-    //         event.preventDefault();
-    //     });
-    // });
-
-    // $(function() {
-    //     $('.rr-nav').resize(function(e) {
-    //         console.log('width: %s, height: %s', $(this).width(), $(this).height());
-    //     });
-    //     // $(".rr-nav").resize(function() {
-    //     //     console.log('.');
-
-    //     // });
-    // });
-
-
-    // $(".rr-nav").resize(function(e) {
-
-    //     // $('#wwqq').quickfit();
-    //     // do something when #unicorns element resizes
-    //     $('#jfjf').textfill({
-    //         debug: true,
-    //         success: function() {
-
-    //         },
-    //         fail: function(a) {
-
-
-    //         }
-    //     });
-    // });
+    self.scrolled = function(vm, event) {
+        event.currentTarget.scrollLeft -= (event.deltaY * 60);
+    };
 
     self.toggle_Nav = function(event) {
         console.log(event);
@@ -48,23 +18,9 @@ var headerViewmodel = function(data) {
         }).done(function(data) {
             $(".rr-nav-content").html("");
             for (var f in data) {
-                var str = "<img class='rr-nav-content-img' src='" + data[f].preview.medium + "' />";
-                $(".rr-nav-content").append(str);
+                self.items.push(data[f].preview.medium);
             }
             $(".rr-nav").toggleClass('rr-nav-open');
-
-            // function adjustHeights(elem) {
-            //     var fontstep = 2;
-            //     if ($(elem).height() > $(elem).parent().height() || $(elem).width() > $(elem).parent().width()) {
-            //         $(elem).css('font-size', (($(elem).css('font-size').substr(0, 2) - fontstep)) + 'px').css('line-height', (($(elem).css('font-size').substr(0, 2))) + 'px');
-            //         adjustHeights(elem);
-            //     }
-            // }
         });
     };
-
-    // $("#blub").click(function() {
-
-    // });
-
 };
