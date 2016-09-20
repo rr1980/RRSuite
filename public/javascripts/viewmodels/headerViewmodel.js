34a818644f0d922d2dd9f9583e3170eb3b1d3395
rr1980 <rener1980@googlemail.com>
2016-09-20 18:03:09 +0200

    a

--- public/javascripts/viewmodels/headerViewmodel.js
+++ public/javascripts/viewmodels/headerViewmodel.js
@@ -7,22 +7,16 @@ var headerViewmodel = function(data) {
         event.currentTarget.scrollLeft -= (event.deltaY * 60);
     };
 
-    self.toggle_Nav = function(event) {
-        console.log(event);
-
-
+    self.toggle_Nav = function() {
         $.ajax({
             url: "/getFollows",
             type: "GET",
             cahce: false
         }).done(function(data) {
-
             for (var f in data) {
-                // console.debug(data[f].channel.display_name);
                 self.items.push(data[f]);
             }
             $(".rr-nav").toggleClass('rr-nav-open');
-            console.debug(self.items()[0]);
         });
 
     };
