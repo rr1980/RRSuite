bca47d20b2feb6f8d245da19891662a3e7e25900
rr1980 <rener1980@googlemail.com>
2016-09-20 22:51:59 +0200

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var g = require('../global');
 
 
-
 g.router.get('/', function(req, res, next) {
     g.twitch_api.Auth(req, res, function() {
         res.render('_layout', {
@@ -10,67 +9,19 @@ g.router.get('/', function(req, res, next) {
             loadScripts: g.bundler.loadScripts(),
             loadCss: g.bundler.loadCss()
         });
+    }, function(msg) {
+        console.log(msg);
 
-    })
+    });
+});
+
+g.router.get('/twitch*', function(req, res, next) {
+    g.twitch_api.set_auth(req, res, next, function() {
+        res.redirect("/");
+    }, function(msg) {
+        console.log(msg);
+    });
 });
 
 
-
-
-g.router.get('/', function(req, res, next) {
-            g.twitch_api.Auth(req, res, function() {
-                res.render('_layout', {
-                    // content: "index.ejs",
-                    //model: indexController.model(),
-                    loadScripts: g.bundler.loadScripts(),
-                    loadCss: g.bundler.loadCss()
-                });
-            }, function(msg) {
-                console.log(msg);
-
-
-                res.render('_layout', {
-                    // content: "index.ejs",
-                    //model: indexController.model(),
-                    loadScripts: g.bundler.loadScripts(),
-                    loadCss: g.bundler.loadCss()
-                });
-
-
-
-            }, function(msg) {
-                console.log(msg);
-
-            });
-
-            g.router.get('/', function(req, res, next) {
-                if (g.rdy) {
-                    if (g.twitch_api.isAuth()) {
-
-                        res.render('_layout', {
-                            // content: "index.ejs",
-                            //model: indexController.model(),
-                            loadScripts: g.bundler.loadScripts(),
-                            loadCss: g.bundler.loadCss()
-                        });
-                    } else {
-                        res.redirect(g.twitch_api.AuthUrl());
-                    }
-                } else {
-                    res.send('Server not rdy!');
-                }
-
-            });
-
-            g.router.get('/twitch*', function(req, res, next) {
-                if (req.query.code !== null) {
-                    g.twitch_api.Init(req.query.code);
-                    res.redirect("/");
-                } else {
-                    console.log('NOPE!!! /index.js /twitch no code in query');
-                    res.send("NOPE!!! /index.js /twitch no code in query");
-                }
-            });
-
-
-            module.exports = g.router;
+module.exports = g.router;
